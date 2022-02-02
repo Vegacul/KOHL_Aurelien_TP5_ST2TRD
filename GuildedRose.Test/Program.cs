66106875d6ace1ebc3b4d405225520b49dd13900
Aurélien <aurelien.kohl@efrei.net>
2022-02-02 15:11:45 +0100

    COmmit Creation projet Test

---
+++ GuildedRose.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuildedRose.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
